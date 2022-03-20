49a85ee2109995818a415e541dc1bc507cffb40f
ArGoN <70173258+ArGoNArGoN@users.noreply.github.com>
2022-03-20 17:44:44 +0300

    Создание базовых папок и классов для MVVM

---
+++ project/project/project/Models/BaseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project.Models
+{
+    /// <summary>
+    /// Базовая сужность
+    /// </summary>
+    public abstract class BaseModel
+    {
+        public BaseModel()
+        {
+        }
+    }
+}
---
+++ project/project/project/ViewModels/BaseViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace project.ViewModels
+{
+    /// <summary>
+    /// Базовая сущность для ViewModel
+    /// </summary>
+    public class BaseViewModel
+        : INotifyPropertyChanged
+    {
+        protected String titlePage;
+
+        /// <summary>
+        /// Название страницы
+        /// </summary>
+        public virtual String TitlePage
+        {
+            get { return titlePage; }
+            protected set 
+            {
+                titlePage = value;
+                OnPropertyChanged(nameof(TitlePage)); /// обновляет значение
+            }
+        }
+        /// <summary>
+        /// событие изменения свойства
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public BaseViewModel()
+        {
+        }
+
+        /// <summary>
+        /// Возникает при изменении свойства
+        /// </summary>
+        /// <param name="propertyName">Название свойства</param>
+        protected void OnPropertyChanged(String propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
