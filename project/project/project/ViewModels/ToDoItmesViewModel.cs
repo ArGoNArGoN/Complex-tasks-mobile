29680b6b3b3f7df9f5e1843c54c6215a8d9acc46
ArGoN <70173258+ArGoNArGoN@users.noreply.github.com>
2022-03-21 01:03:43 +0300

    Создание модели ToDo и ViewModel

---
+++ project/project/project/ViewModels/ToDoItmesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using project.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace project.ViewModels
+{
+	/// <summary>
+	/// Связывает коллекцию задач с View
+	/// </summary>
+	public class ToDoItmesViewModel<T, V>
+		: BaseViewModel
+	{
+	}
+}
---
+++ project/project/project/ViewModels/ToDoListViewModel.cs
@@ -0,0 +1,50 @@
+﻿using project.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace project.ViewModels
+{
+    public class ToDoListViewModel<T, V>
+        : BaseViewModel
+        where T : ToDoViewModel<V>
+        where V : BaseToDoModel, new()
+    {
+
+		private String listName = "";
+
+		public ToDoListViewModel()
+			: base()
+		{
+			Items = new ObservableCollection<T>();
+		}
+
+		/// <summary>
+		/// Название списка
+		/// </summary>
+		public String ListName
+		{
+			get { return listName + ":"; }
+			set { listName = value + ""; OnPropertyChanged(nameof(ListName)); }
+		}
+		/// <summary>
+		/// Коллекция задач
+		/// </summary>
+		public ObservableCollection<T> Items { get; }
+
+		/// <summary>
+		/// Присваивает новую коллекцию элементов
+		/// </summary>
+		/// <param name="enumerable">коллекция из задач</param>
+		public virtual void SetItmes(IEnumerable<T> enumerable)
+		{
+			Items.Clear();
+
+			if (enumerable is null)
+				return;
+
+			foreach (var item in enumerable)
+				Items.Add(item);
+		}
+	}
+}
